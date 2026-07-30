3ecb443d4059b0b39fd98c41adaeb0d943d2cacb
abhishek2006al <lokareabhishek260@gmail.com>
2026-07-30 14:08:48 +0530

    Add files via upload

---
+++ starpatern.py
@@ -0,0 +1,16 @@
+print("=================Star pattern=================")
+
+for i in range(1,6):
+      for j in range(i):
+            print("#",end=" ")
+      print()
+
+print()      
+    
+for i in range(1,6):
+      for j in range(1,i+1):
+            print(j,end=" ")
+      print()     
+      
+    
+
